0771a5a26f3259f8e5d397277924cd9e1d462306
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-06 08:43:18 -0600

    m8-common: Update blobs from M
    * Blobs belonging to HTC firmware 5.07.1700.6
      (htc/m8_google/htc_m8:6.0/MRA58K.H6/648564:user/release-keys)
    * Update perf prop
    * Update variant script for DS devices (now sharing the same blobs)
    
    Change-Id: I2395c95670b8269b6d675af38ff79c18b726b856

--- system_prop.mk
+++ system_prop.mk
@@ -5,3 +5,7 @@
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.maxopen=3
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=/vendor/lib/libqti-perfd-client.so
