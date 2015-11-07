674c5cbf857e711fa94805766d8078b24e710ca9
Ethan Chen <intervigil@gmail.com>
2015-11-06 19:23:06 -0500

    m8-common: Add CameraParameters for CameraParameters_EXT
    Change-Id: I3290aa5b72446898641ebf91cc019ae99192bb40

--- camera/Android.mk
+++ camera/Android.mk
@@ -16,3 +16,18 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 #include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := \
+    frameworks/av/include
+
+LOCAL_SRC_FILES := \
+    CameraParameters.cpp \
+    CameraParameters_EXT.cpp
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE := libcamera_parameters_ext
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_STATIC_LIBRARY)
