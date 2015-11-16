c3c863a97c07d0e82b69092f6f125935b419daaf
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-15 21:31:33 -0600

    m8: bring sepolicy in from msm8974-common
    Change-Id: I0ccf2de8d2ce573d94eba78bbb3cf55d67aa3c4b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,7 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_NFC_HAL_SUFFIX := msm8974
 
 # SELinux
+include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/htc/m8-common/sepolicy
 
 # Releasetools
