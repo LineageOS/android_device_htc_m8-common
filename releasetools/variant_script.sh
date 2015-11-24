c3893338bf1d1cf71b654988f9441105f424846f
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-23 21:37:19 -0600

    m8-common: commonize all the things
    Change-Id: If216cadf1a6cf431acfc3e43d7350877395b7e8c

--- releasetools/variant_script.sh
+++ releasetools/variant_script.sh
@@ -2,40 +2,27 @@
 
 set -e
 
-# Detect variant and create symlinks to its specific-blobs
+# Detect variant
 modelid=`getprop ro.boot.mid`
 
 case $modelid in
     "0P6B20000") variant="vzw" ;;
     "0P6B70000") variant="spr" ;;
-    "0P6B61000") variant="dug" ;;
-    "0P6B64000") variant="dug" ;;
-    "0P6B68000") variant="dug" ;;
+    "0P6B61000"|"0P6B64000"|"0P6B68000") variant="dug" ;;
     "0P6B41000") variant="dwg" ;;
     "0P6B13000") variant="gsm"; tmo="true" ;;
     *)           variant="gsm" ;;
 esac
 
-# CDMA variants need a slight change to their gps.conf
-if [ "$variant" == "vzw" ] || [ "$variant" == "spr" ] || [ "$variant" == "dwg" ]; then
-  sed -i "s|PHONE_TYPE=UMTS|PHONE_TYPE=CDMA|g" /system/etc/gps.conf
-fi
-
-# Skip symlink creation for Dual SIM variants because blobs are already in the proper location
-if [ "$variant" == "vzw" ] || [ "$variant" == "spr" ] || [ "$variant" == "gsm" ]; then
-  basedir="/system/blobs/$variant/"
-  cd $basedir
-  chmod 755 bin/*
-  find . -type f | while read file; do ln -s $basedir$file /system/$file ; done
-fi
-
 # Create modem firmware links based on the currently installed modem
 mkdir -p /firmware/radio
 mount -o shortname=lower -t vfat /dev/block/platform/msm_sdcc.1/by-name/radio /firmware/radio
 
-# Prefer an mba.* pair if one exists
+# GPE 6.0 baseband gets priority here, otherwise prefer an mba.* pair if one exists
 # If not, find the a7b* pair with the highest "number" (unless you're T-Mo)
-if [ "$tmo" == "true" ] && [ -f "/firmware/radio/a7b80e1.mdt" ]; then
+if cat /firmware/radio/radiover.cfg | grep -q "SSD:1.23.213311491.A13G"; then
+  base="/firmware/radio/a7b80e1"
+elif [ "$tmo" == "true" ] && [ -f "/firmware/radio/a7b80e1.mdt" ]; then
   base="/firmware/radio/a7b80e1"
 elif [ -f "/firmware/radio/mba.mdt" ]; then
   base="/firmware/radio/mba"
