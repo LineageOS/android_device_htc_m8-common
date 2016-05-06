41842e4be37ce58f9d56eb197574a5c285530659
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-05-06 15:43:22 -0500

    m8: simplify mba firmware link creation
    * M firmware or gtfo
    
    Change-Id: I5632dcd092bbe7bfaa859be48d84247f2d99668f

--- releasetools/variant_script.sh
+++ releasetools/variant_script.sh
@@ -29,26 +29,4 @@ if [ "$variant" == "vzw" ] || [ "$variant" == "spr" ] || [ "$variant" == "gsm" ]
   find . -type f | while read file; do ln -s $basedir$file /system/$file ; done
 fi
 
-# Create modem firmware links based on the currently installed modem
-mkdir -p /firmware/radio
-mount -o shortname=lower -t vfat /dev/block/platform/msm_sdcc.1/by-name/radio /firmware/radio
-
-# Prefer an mba.* pair if one exists
-# If not, find the a7b* pair with the highest "number" (unless you're T-Mo)
-if [ "$tmo" == "true" ] && [ -f "/firmware/radio/a7b80e1.mdt" ]; then
-  base="/firmware/radio/a7b80e1"
-elif [ -f "/firmware/radio/mba.mdt" ]; then
-  base="/firmware/radio/mba"
-elif ls /firmware/radio/a7b*.mdt 1> /dev/null 2>&1; then
-  base=`ls /firmware/radio/a7b*.mdt | sort -r | head -n 1 | sed "s|.mdt||g"`
-fi
-
-ln -s $base.mdt /system/vendor/firmware/mba.mdt
-ln -s $base.b00 /system/vendor/firmware/mba.b00
-
-if [ ! -f "/system/vendor/firmware/mba.mdt" ]; then
-  exit 1
-fi
-
-umount /firmware/radio
 exit 0
