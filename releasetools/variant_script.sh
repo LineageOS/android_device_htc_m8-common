5cc454c4155a19a6a2a5b4b05be8aa6706a1629a
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-07-18 08:58:52 -0500

    m8: remove gps.conf hack
    * we'll ship the correct variant ones from now on
    
    Change-Id: If5226ff0617d79a0cfa2553263dbe662b8604c9f

--- releasetools/variant_script.sh
+++ releasetools/variant_script.sh
@@ -16,11 +16,6 @@ case $modelid in
     *)           variant="gsm" ;;
 esac
 
-# CDMA variants need a slight change to their gps.conf
-if [ "$variant" == "vzw" ] || [ "$variant" == "spr" ] || [ "$variant" == "dwg" ]; then
-  sed -i "s|PHONE_TYPE=UMTS|PHONE_TYPE=CDMA|g" /system/etc/gps.conf
-fi
-
 # Skip symlink creation for Dual SIM variants because blobs are already in the proper location
 if [ "$variant" == "vzw" ] || [ "$variant" == "spr" ] || [ "$variant" == "gsm" ]; then
   basedir="/system/blobs/$variant/"
