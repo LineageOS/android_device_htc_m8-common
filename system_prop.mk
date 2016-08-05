0b38e58b4b9d537e3c9d16b971b379441bc15818
Scott Mertz <scott@cyngn.com>
2016-08-05 10:07:13 -0700

    m8-common: add first_api_level for CTS
    Change-Id: I4a34d5116cffc648c52a655f8349135a0a5aca49

--- system_prop.mk
+++ system_prop.mk
@@ -2,6 +2,10 @@
 # Common system properties for m8 and m8d
 #
 
+# API (for CTS backward compatibility)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.first_api_level=19
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.maxopen=3
