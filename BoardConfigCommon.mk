546f5364b6ab125df6a6ee277ee00abdee7a70bd
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-08-26 08:54:24 -0500

    [DO NOT MERGE] fixes to build N
    Change-Id: Ieaf5e84a32a8fd623e01cbb8f9e35f2289b75bf9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,8 +43,8 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_NFC_HAL_SUFFIX := msm8974
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/htc/m8-common/sepolicy
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += device/htc/m8-common/sepolicy
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/m8-common/releasetools
