791d86e0df7f46d4a028461c1b7765ea14d44ffe
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-09-10 12:08:17 -0500

    m8: remove deprecated cflags
    Change-Id: Ice66351c90ed3e186072ee87201f883901b66d0b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,8 +36,6 @@ TARGET_BOARD_INFO_FILE ?= device/htc/m8-common/board-info.txt
 TARGET_SPECIFIC_HEADER_PATH += device/htc/m8-common/include
 
 # Camera
-COMMON_GLOBAL_CFLAGS += -DHTC_CAMERA_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND='{"htc.camera.sensor.", AID_CAMERA, 0}, {"camera.4k2k.", AID_MEDIA, 0},'
 TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY := libcamera_parameters_ext
 USE_DEVICE_SPECIFIC_CAMERA := true
 
