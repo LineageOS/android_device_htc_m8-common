f4b0e134c9383e63fb8168e421770f213181a87d
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:01:07 -0700

    m8-common: Move Lineage specific overlays to overlay-lineage
    Change-Id: I07287cfeaa425419d0cd9f5783773107a12a699a

--- m8-common.mk
+++ m8-common.mk
@@ -23,7 +23,9 @@ $(call inherit-product, device/htc/msm8974-common/msm8974-common.mk)
 -include $(LOCAL_PATH)/system_prop.mk
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += device/htc/m8-common/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
