07b3177fa1a24348a526148628a09b6cd29d8615
Kevin F. Haggerty <haggertk@lineageos.org>
2017-09-13 18:10:25 +0100

    m8-common: releasetools: Copy variant blobs instead of creating symlinks
     * Just because we need the libril shim and doing that for each variant
       is non-scaleable
    
    Change-Id: I5d420ff1b6a2c5c9b8dfa33d9c68f4ad8a7b9348

--- releasetools/variant_script.sh
+++ releasetools/variant_script.sh
@@ -1,8 +1,36 @@
 #!/sbin/sh
+#
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -e
 
-# Detect variant and create symlinks to its specific-blobs
+# Helper functions
+better_copy()
+{
+  cp -dp "$1" "$2"
+  # symlinks don't have a context
+  if [ ! -L "$1" ]; then
+    # it is assumed that every label starts with 'u:object_r' and has no white-spaces
+    local context=`ls -Z "$1" | grep -o 'u:object_r:[^ ]*' | head -1`
+    chcon -v "$context" "$2"
+  fi
+}
+
+# Detect variant and copy its specific-blobs
 modelid=`getprop ro.boot.mid`
 
 case $modelid in
@@ -16,12 +44,24 @@ case $modelid in
     *)           variant="gsm" ;;
 esac
 
-# Skip symlink creation for Dual SIM variants because blobs are already in the proper location
+# Skip copying blobs in case of Dual SIM variants because the files are already in the proper location
 if [ "$variant" == "vzw" ] || [ "$variant" == "spr" ] || [ "$variant" == "gsm" ]; then
   basedir="/system/blobs/$variant/"
-  cd $basedir
-  chmod 755 bin/*
-  find . -type f | while read file; do ln -s $basedir$file /system/$file ; done
+  if [ -d $basedir ]; then
+    cd $basedir
+
+    for file in `find . -type f` ; do
+      mkdir -p `dirname /system/$file`
+      better_copy $file /system/$file
+    done
+
+    for file in bin/* ; do
+      chmod 755 /system/$file
+    done
+  else
+    echo "Expected source directory does not exist!"
+    exit 1
+  fi
 fi
 
 exit 0
