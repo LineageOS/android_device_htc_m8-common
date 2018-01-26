998b313e6e620d68e245a530b5124bd2431e280d
Bruno Martins <bgcngm@gmail.com>
2018-01-26 17:51:28 +0000

    m8-common: Move feature configs to vendor
    Change-Id: Ia90e310aad4b0c32c8ac929b56e328d132bde160

--- m8-common.mk
+++ m8-common.mk
@@ -30,9 +30,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-    $(LOCAL_PATH)/configs/com.htc.software.market.xml:system/etc/permissions/com.htc.software.market.xml
+    frameworks/native/data/etc/android.hardware.consumerir.xml:system/vendor/etc/permissions/android.hardware.consumerir.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/vendor/etc/permissions/android.hardware.telephony.cdma.xml \
+    $(LOCAL_PATH)/configs/com.htc.software.market.xml:system/vendor/etc/permissions/com.htc.software.market.xml
 
 # Camera
 PRODUCT_PACKAGES += \
