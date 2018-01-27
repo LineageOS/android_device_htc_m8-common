e96989b9c57ef9cd6daa103b90608876c3a4423c
Bruno Martins <bgcngm@gmail.com>
2018-01-27 01:48:49 +0000

    m8-common: Build IR HAL service
    Change-Id: I77ed1ef076d4ea556b73172165e134305e33dbbf

--- m8-common.mk
+++ m8-common.mk
@@ -48,7 +48,8 @@ PRODUCT_PACKAGES += \
 
 # IRDA
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
 
 PRODUCT_BOOT_JARS += \
     htcirlibs
