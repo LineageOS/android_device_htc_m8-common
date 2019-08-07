b63ee1078af4f8bf03c4db9855d9a7d1768d2ebf
Bruno Martins <bgcngm@gmail.com>
2019-08-07 01:15:33 +0100

    m8-common: camera: Do the shuffle with CameraParameters
     * Up until N it was possible for devices to hook extra classes
       and symbols into libcamera_client. Starting with O, several
       makefiles were converted to blueprints, camera headers changed
       place and overriding headers became almost impossible.
       Due to the nature of HTC's MSM8974 camera HAL, which expects
       CameraParameters to inherit from CameraParameters_EXT, injecting
       the custom CameraParameters code like in the past now resulted
       in mediaserver fatal crashes due to mismatched headers.
       Workaround such issues by shipping a kind of shim and make
       the camera HAL happy.
    
    Change-Id: I85055e485653e8e93424428fd0dc429f8c76f94f

--- extract-files.sh
+++ extract-files.sh
@@ -52,6 +52,14 @@ if [ -z "$SRC" ]; then
     SRC=adb
 fi
 
+function blob_fixup() {
+    case "${1}" in
+    vendor/lib/hw/camera.vendor.msm8974.so)
+        patchelf --replace-needed "libcamera_client.so" "libcamera_client_htc.so" "${2}"
+        ;;
+    esac
+}
+
 # Initialize the helper for common device
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
 
