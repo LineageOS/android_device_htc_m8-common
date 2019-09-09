4c47fdad2a0c77a8291bc369e5c191086f8d8b50
Bruno Martins <bgcngm@gmail.com>
2019-09-09 16:43:30 +0200

    m8-common: Set shipping API level in the common product makefile
    It's not meant to be set as a board config flag. Simply do it by
    inheriting the makefile that now also exists for devices launched
    with KitKat.
    
    Change-Id: I2ca1f9d207be90c58495b3e20c5e2275e09b18bc

--- m8-common.mk
+++ m8-common.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2015-2016 The CyanogenMod Project
-#               2017-2018 The LineageOS Project
+#               2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 # limitations under the License.
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
 $(call inherit-product-if-exists, vendor/htc/m8-common/m8-common-vendor.mk)
 
 # Inherit from msm8974-common
