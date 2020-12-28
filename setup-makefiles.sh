8d850e47ef0565394ef4117f716a08479a7e79d7
Bruno Martins <bgcngm@gmail.com>
2020-12-28 10:34:04 +0000

    m8-common: Align with extraction-utils templates
    Change-Id: Ifbf18f79a1d0f173cb5b83bdf0178db2a5f1373e

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -21,4 +11,4 @@ set -e
 export BOARD_COMMON=msm8974-common
 export DEVICE_COMMON_GUARDS="m8 m8d"
 
-./../$BOARD_COMMON/setup-makefiles.sh $@
+"./../../${VENDOR}/${BOARD_COMMON}/setup-makefiles.sh" "$@"
