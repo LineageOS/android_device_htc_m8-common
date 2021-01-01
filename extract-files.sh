27dddf1b74296a6a6dfe28caead8da2605daff94
Bruno Martins <bgcngm@gmail.com>
2021-01-01 23:46:17 +0000

    m8-common: Rewrite integration with scripts in msm8974-common
    Change-Id: I22b9589ebf089d678168e6f167d96799172e45a5

--- extract-files.sh
+++ extract-files.sh
@@ -18,40 +18,6 @@
 
 set -e
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-LINEAGE_ROOT="$MY_DIR"/../../..
-
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Default to sanitizing the vendor folder before extraction
-CLEAN_VENDOR=true
-
-while [ "$1" != "" ]; do
-    case $1 in
-        -n | --no-cleanup )     CLEAN_VENDOR=false
-                                ;;
-        -s | --section )        shift
-                                SECTION=$1
-                                CLEAN_VENDOR=false
-                                ;;
-        * )                     SRC=$1
-                                ;;
-    esac
-    shift
-done
-
-if [ -z "$SRC" ]; then
-    SRC=adb
-fi
-
 function blob_fixup() {
     case "${1}" in
     vendor/lib/hw/camera.vendor.msm8974.so)
@@ -60,17 +26,8 @@ function blob_fixup() {
     esac
 }
 
-# Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
+export BOARD_COMMON=msm8974-common
+export DEVICE_COMMON=m8-common
+export DEVICE_SPECIFIED_COMMON_DEVICE="m8 m8d"
 
-extract "$MY_DIR"/common-proprietary-files.txt "$SRC" "$SECTION"
-
-# Reinitialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
-
-extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
-extract "$MY_DIR"/../$DEVICE/device-proprietary-files.txt "$SRC" "$SECTION"
-
-./../msm8974-common/extract-files.sh $@
-
-"$MY_DIR"/setup-makefiles.sh
+./../$BOARD_COMMON/extract-files.sh $@
