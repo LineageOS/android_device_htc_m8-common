ea64ba36bd96989c1bc7735b290d76480bd223db
Kevin F. Haggerty <haggertk@lineageos.org>
2021-04-09 08:18:37 -0600

    m8-common: Don't let builds complete without vendor tree
    * There is zero reason to ever build without blobs. We've even seen
      this situation with official builds from our servers. It's always
      better for a build to fail than it is for it to produce something
      that has no chance at working.
    
    Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a

--- m8-common.mk
+++ m8-common.mk
@@ -16,7 +16,7 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
-$(call inherit-product-if-exists, vendor/htc/m8-common/m8-common-vendor.mk)
+$(call inherit-product, vendor/htc/m8-common/m8-common-vendor.mk)
 
 # Inherit from msm8974-common
 $(call inherit-product, device/htc/msm8974-common/msm8974-common.mk)
